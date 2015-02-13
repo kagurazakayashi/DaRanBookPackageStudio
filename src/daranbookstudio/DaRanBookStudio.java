f7843c2b1cc1cc8c087a77c0d4fd72f480051813
神楽坂雅詩 <cyscxchope@gmail.com>
2015-02-13 12:20:03 +0800

    完成UI

---
+++ src/daranbookstudio/DaRanBookStudio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daranbookstudio;
+
+/**
+ *
+ * @author 雅詩
+ */
+public class DaRanBookStudio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        WelcomeWindow ww = new WelcomeWindow();
+        ww.setVisible(true);
+    }
+    
+}
